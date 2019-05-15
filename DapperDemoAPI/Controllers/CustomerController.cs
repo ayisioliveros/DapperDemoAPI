4238e195bc6a2143a557db3ddee2361912ea3f36
maria_oliveros <maria.oliveros@ghl.com>
2019-05-15 09:36:16 +0800

    Initial Commit Signed-off-by: maria_oliveros <maria.oliveros@ghl.com>

---
+++ DapperDemoAPI/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using DapperDemoAPI.DAL;
+using DapperDemoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DapperDemoAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private CustomerRespository _ourCustomerRespository = new CustomerRespository();
+
+        [Route("Customers")]
+        [HttpGet]
+        public List<Customer> Get()
+        {
+            return _ourCustomerRespository.GetCustomers(10, "ASC");
+        }
+
+        [Route("Customers/{id}")]
+        [HttpGet]
+        public Customer Get(int id)
+        {
+            return _ourCustomerRespository.GetSingleCustomer(id);
+        }
+
+        public bool Post([FromBody]Customer ourCustomer)
+        {
+            return _ourCustomerRespository.InsertCustomer(ourCustomer);
+        }
+
+        [Route("Customers")]
+        [HttpPut]
+        public bool Put([FromBody]Customer ourCustomer)
+        {
+            return _ourCustomerRespository.UpdateCustomer(ourCustomer);
+        }
+
+        [Route("Customers/{id}")]
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return _ourCustomerRespository.DeleteCustomer(id);
+        }
+    }
+}
---
+++ DapperDemoAPI/DAL/ICustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using DapperDemoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemoAPI.DAL
+{
+    internal interface ICustomerRepository
+    {
+        List<Customer> GetCustomers(int amount, string sort);
+
+        Customer GetSingleCustomer(int customerId);
+
+        bool InsertCustomer(Customer ourCustomer);
+
+        bool DeleteCustomer(int customerId);
+
+        bool UpdateCustomer(Customer ourCustomer);
+    }
+}
